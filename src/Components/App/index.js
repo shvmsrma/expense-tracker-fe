9f713c42a8fe5b44c62606bd6d135caed606d9bc
Shivam Sharma <shvmsrma@gmail.com>
2021-09-07 10:18:43 +0530

    TEST

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,17 +1,13 @@
-
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return (<div>
-            Hi
-        </div>  );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Hia</div>;
+  }
 }
- 
+
 export default App;
